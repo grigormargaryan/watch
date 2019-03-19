c63558963f17beb5c446454290f62e206ca17740
Grigor <margaryanmgrigor@gmail.com>
2019-03-19 11:34:47 +0400

    meta tags

--- resources/views/user/include/head.blade.php
+++ resources/views/user/include/head.blade.php
@@ -9,6 +9,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
+    <meta name="jamacuycner" content="ejan jamacuyc">
+    <meta name="jamacuyc" content="dzerqi jamer">
+    <meta name="kanaci" content="kanaci jamer">
+    <meta name="txamardu" content="txamardu jamer">
+    <meta name="nverner" content="nverner">
+
     <title>2ntOne</title>
 
     <link rel="stylesheet" type="text/css" href="{{ asset('usercss/bootstrap.min.css') }}">
